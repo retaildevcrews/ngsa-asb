731972a21f6d24f6f91a8fbbceea51cb271795c6
Akinlolu Akindele <AAkindele@users.noreply.github.com>
2022-07-20 19:56:38 +0000

    fix issue with k3d setup. got order of operation wrong

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# install k3d
+wget -q -O - https://raw.githubusercontent.com/rancher/k3d/main/install.sh | TAG=v5.4.4 bash
+
 # create local registry
 docker network create k3d
 k3d registry create registry.localhost --port 5000
--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# install k3d
-wget -q -O - https://raw.githubusercontent.com/rancher/k3d/main/install.sh | TAG=v5.4.4 bash
-
 # install clusterctl
 curl -L https://github.com/kubernetes-sigs/cluster-api/releases/download/v1.1.5/clusterctl-linux-amd64 -o clusterctl
 chmod +x ./clusterctl
