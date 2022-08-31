cb42e89c5c042d955c75a1346f5404c46ac9d1c3
Siva Mullapudi <sivamu@microsoft.com>
2022-08-31 09:13:54 -0500

    Update devcontainer base image

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 
-# install k3d
-wget -q -O - https://raw.githubusercontent.com/rancher/k3d/main/install.sh | TAG=v5.4.4 bash
-
-# install clusterctl
+# install clusterctl (Cluster API)
 curl -L https://github.com/kubernetes-sigs/cluster-api/releases/download/v1.1.5/clusterctl-linux-amd64 -o clusterctl
 chmod +x ./clusterctl
 sudo mv ./clusterctl /usr/local/bin/clusterctl
-
-# create local registry
-docker network create k3d
-k3d registry create registry.localhost --port 5000
-docker network connect k3d k3d-registry.localhost
