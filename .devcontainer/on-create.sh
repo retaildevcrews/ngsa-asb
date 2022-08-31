e8f3916016277fddfd281fcc4c92206ac7e93ee0
sivamu <67930841+sivamu@users.noreply.github.com>
2022-08-31 13:56:02 -0500

    Update ASB devcontainer  (#112)
    * Update devcontainer base image
    
    * Update build flags
    
    * update oncreate command
    
    * Update k3d in on-create
    
    * fix latest working flux version

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -1,14 +1,31 @@
 #!/bin/sh
 
-# install k3d
-wget -q -O - https://raw.githubusercontent.com/rancher/k3d/main/install.sh | TAG=v5.4.4 bash
+echo "on-create started" >> $HOME/status
 
-# install clusterctl
+# Change shell to zsh for vscode
+sudo chsh --shell /bin/zsh vscode
+
+# Install k3d > 5.0.1
+k3d --version | grep -Eo '^k3d version v5...[1-9]$' > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    # Means we don't have proper k3d version
+    # Install v5.4.6
+    echo "Installing k3d v5.4.6"
+    wget -q -O - https://raw.githubusercontent.com/rancher/k3d/main/install.sh | TAG=v5.4.6 sudo bash
+fi
+
+# Create Docker Network for k3d
+docker network create k3d
+
+# Create local container registry
+k3d registry create registry.localhost --port 5000
+
+# Connect to local registry
+docker network connect k3d k3d-registry.localhost
+
+# install clusterctl (Cluster API)
 curl -L https://github.com/kubernetes-sigs/cluster-api/releases/download/v1.1.5/clusterctl-linux-amd64 -o clusterctl
 chmod +x ./clusterctl
 sudo mv ./clusterctl /usr/local/bin/clusterctl
 
-# create local registry
-docker network create k3d
-k3d registry create registry.localhost --port 5000
-docker network connect k3d k3d-registry.localhost
+echo "on-create completed" > $HOME/status
