472ee073e59e35b4c6cf7683139285c14f705495
Kushal Azim Ekram <mekram@microsoft.com>
2022-08-31 13:45:50 -0500

    Install flux cli

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -8,6 +8,13 @@ curl -L https://github.com/kubernetes-sigs/cluster-api/releases/download/v1.1.5/
 chmod +x ./clusterctl
 sudo mv ./clusterctl /usr/local/bin/clusterctl
 
+# install latest flux in ~/.local/bin
+curl -s https://fluxcd.io/install.sh |  bash -s - ~/.local/bin
+# install flux completions for bash
+echo '. <(flux completion bash)' >> ~/.bashrc
+# install flux completions for zsh
+echo '. <(flux completion zsh)' >> ~/.zshrc
+
 # create local registry
 docker network create k3d
 k3d registry create registry.localhost --port 5000
