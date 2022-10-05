d6a2607043bb888ad09a7f83a8626dbd6f87c4c0
Travis <66956775+TravisFallsMSFT@users.noreply.github.com>
2022-10-05 18:02:35 +0000

    fixed formatting of output

--- scripts/getClusterAdminIDforDeployment.sh
+++ scripts/getClusterAdminIDforDeployment.sh
@@ -4,7 +4,8 @@ function GetClusterAdminID(){
     echo "Getting Cluster Admin ID..."
 
     if az account show -o none; then
-        echo "Your are logged into Azure subscription $(az account show --query name) as user $(az account show --query user.name)"
+        echo "You are in to Azure as user $(az account show --query user.name)"
+        echo "You are logged into Azure subscription $(az account show --query name)"
     else
         az login --use-device-code --output none
     fi
@@ -12,6 +13,7 @@ function GetClusterAdminID(){
     export ASB_CLUSTER_ADMIN_GROUP=4-co
     
     cluster_admin_id=$(az ad group show -g $ASB_CLUSTER_ADMIN_GROUP --query id -o tsv)
+    echo ""
     echo "Clusater Admin ID: $cluster_admin_id"
 }
 
