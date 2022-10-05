eb176730e6cbf4b65c8cdf9d5d61dba88ae49acc
Travis <66956775+TravisFallsMSFT@users.noreply.github.com>
2022-10-05 17:42:43 +0000

    Adding script to get adminID

---
+++ scripts/getClusterAdminIDforDeployment.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function GetClusterAdminID(){
+    echo "Getting Cluster Admin ID..."
+    az login --use-device-code 
+
+    export ASB_CLUSTER_ADMIN_GROUP=4-co
+
+    $clusterAdminId = $(az ad group show -g $ASB_CLUSTER_ADMIN_GROUP --query id -o tsv)
+    echo $clusterAdminId
+}
+
+GetClusterAdminID
