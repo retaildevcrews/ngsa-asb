40f5493da25dbd3bc0e9f292ec438941ec4bb052
Travis <66956775+TravisFallsMSFT@users.noreply.github.com>
2022-10-05 17:57:42 +0000

    fixed output to allow for login check

--- scripts/getClusterAdminIDforDeployment.sh
+++ scripts/getClusterAdminIDforDeployment.sh
@@ -2,11 +2,17 @@
 
 function GetClusterAdminID(){
     echo "Getting Cluster Admin ID..."
-    az login --use-device-code 
+
+    if az account show -o none; then
+        echo "Your are logged into Azure subscription $(az account show --query name)"
+    else
+        az login --use-device-code --output none
+    fi
 
     export ASB_CLUSTER_ADMIN_GROUP=4-co
-
-    az ad group show -g $ASB_CLUSTER_ADMIN_GROUP --query id -o tsv
+    
+    cluster_admin_id=$(az ad group show -g $ASB_CLUSTER_ADMIN_GROUP --query id -o tsv)
+    echo "Clusater Admin ID: $cluster_admin_id"
 }
 
 GetClusterAdminID
