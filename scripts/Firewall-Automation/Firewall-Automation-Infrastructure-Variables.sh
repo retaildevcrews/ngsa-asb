49e6fde1ed842b60dfb716c99e7d7347a4b516ee
gortegaMS <70150605+gortegaMS@users.noreply.github.com>
2022-12-06 20:42:48 +0000

    Removed tenantid and subscriptionid from variables.sh file

--- scripts/Firewall-Automation/Firewall-Automation-Infrastructure-Variables.sh
+++ scripts/Firewall-Automation/Firewall-Automation-Infrastructure-Variables.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-export ASB_FW_Tenant_Id='16b3c013-d300-468d-ac64-7eda0820b6d3'
+export ASB_FW_Tenant_Id=''
 export ASB_DEPLOYMENT_NAME='firewall-automation'
 export ASB_SPOKE_LOCATION='eastus'
 export ASB_ENV='dev'
-export ASB_FW_Subscription_Name='MCAPS-43649-AUS-DEVCREWS'
+export ASB_FW_Subscription_Name=''
 export ASB_FW_Base_NSGA_Name='wcnp'
 export ASB_FW_Base_Automation_System_Name='firewall-automation'
 export ASB_FW_Environment='dev'
